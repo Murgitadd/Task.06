7c11be029b966dc8e03667d334ec03c8079ab488
Murad <145739695+Murgitadd@users.noreply.github.com>
2023-10-09 03:00:00 +0400

    Done Task2.cs
    CustomTrimEnd method does no function as expected.

--- Task2.cs
+++ Task2.cs
@@ -1,16 +1,75 @@
-Console.WriteLine("Enter a three-digit number:");
+using System;
 
-int num = int.Parse(Console.ReadLine());
-
-int yuzluk = (num % 1000) / 100;
-int onluq = (num % 100) / 10;
-int teklik = (num % 10);
-
-if (yuzluk == onluq && onluq == teklik)
+namespace Task_2_Project
 {
-    Console.WriteLine("Butun ededler eynidir.");
-}
-else
-{
-    Console.WriteLine("Eyni olmayan ededler var!");
+    class Program
+    {
+        public static string CustomTrimStart(string word)
+        {
+            char[] arr = new char[word.Length];
+            int j = 0;
+
+            arr[0] = ' '; 
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (arr[0] == 0 || arr[0] == 32)
+                {
+                    arr[0] = word[i];
+                }
+                else if (arr[0] != 0 && arr[0] != 32)
+                {
+                    arr[j] = word[i];
+                    j++;
+                }
+            }
+
+            string response = new string(arr, 0, j);
+            return response;
+        }
+
+        public static string CustomTrimEnd(string word)
+        {
+            char[] arr = new char[word.Length];
+            int j = 0;
+
+            arr[0] = ' ';
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                if (arr[0] == 0 || arr[0] == 32)
+                {
+                    arr[0] = word[i];
+                }
+                else if (arr[0] != 0 && arr[0] != 32)
+                {
+                    arr[j] = word[i];
+                    j++;
+                }
+            }
+
+            string response1 = new string(arr, 0, j);
+            return response1;
+        }
+
+        public static string CustomTrim(string word)
+        {
+            string request = CustomTrimStart(word);
+            string response = CustomTrimEnd(request);
+            return response;
+        }
+
+        public static void Main(string[] args)
+        {
+            string word = "Salam";
+            string response = CustomTrimEnd(word);
+
+            Console.WriteLine(response);
+
+            // TODO: Implement Functionality Here
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
 }
