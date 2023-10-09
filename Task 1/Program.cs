1dd3a0abbe13f29d65e82616fd0b428a3047d9e5
Murad <145739695+Murgitadd@users.noreply.github.com>
2023-10-10 00:21:30 +0400

    Fixed Program.cs

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,89 +1,28 @@
-﻿/*
- * Created by SharpDevelop.
- * User: HP
- * Date: 8.10.2023
- * Time: 20:02
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
 using System;
 
 namespace codek
 {
-	/// <summary>
-	/// Description of Student.
-	/// </summary>
-	public class Student
-	{
-		
-		
-		string Name;
-		string SurName;
-		string Group;
-		double Point;
-		bool isGraduated = false;
-		
-		
-		public Student(string Name, string SurName, string Group, double Point)
-		{
-			
-			this.Name = Name;
-			this.SurName = SurName;
-			this.Group = Group;
-			this.Point = Point;
-			
-			if (Point > 65)
-			{
-				
-				isGraduated = true;
-				
-			}
-			
-		}
-		
-		public void GetInfo()
-		{
-			
-			Console.WriteLine("Name: " + Name);
-			Console.WriteLine("SurName: " + SurName);
-			Console.WriteLine("Group: " + Group);
-			Console.WriteLine("Point: " + Point);
-			Console.WriteLine("Graduation: " + isGraduated);
-			
-		}
-		
-		public void CheckGraduation()
-		{
-			if (isGraduated)
-			{
-				Console.WriteLine(Name + " has graduated with " + Point + " points! ");
-			}
-			else 
-			{
-				Console.WriteLine(Name + " could not graduate with " + Point + " points... T-T ");
-			}
-		}
-		
-		
-		public void GetDiplomDegree()
-		{
-			if (Point >= 90)
-			{
-				Console.WriteLine(Name + " achieved High Honor!!!");
-			}
-			else if (Point >= 80)
-			{
-				Console.WriteLine(Name + " achieved Honor!");
-			}
-			else if (Point >= 65)
-			{
-				Console.WriteLine(Name + " achieved felicitation.");
-			}
-			else
-			{
-				Console.WriteLine(Name + " failed graduation...");
-			}
-		}
-		
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        
+            Student student1 = new Student("John", "Doe", "GroupA", 70);
+            Student student2 = new Student("Alice", "Smith", "GroupB", 85);
+
+            Console.WriteLine("Student 1 Info:");
+            student1.GetInfo();
+
+            Console.WriteLine("\nStudent 2 Info:");
+            student2.GetInfo();
+
+            Console.WriteLine("\nChecking Graduation Status:");
+            student1.CheckGraduation();
+            student2.CheckGraduation();
+
+            Console.WriteLine("\nDiploma Degrees:");
+            student1.GetDiplomDegree();
+            student2.GetDiplomDegree();
+
+        }
+    }
 }
